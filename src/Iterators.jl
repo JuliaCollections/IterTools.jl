1ce88987023fbb62b61c4aded0581969064148e8
getzdan <getzdan@gmail.com>
2016-09-21 13:18:46 -0400

    Add empty return for subsets larger than base set. (#64)

--- src/Iterators.jl
+++ src/Iterators.jl
@@ -472,7 +472,7 @@ length(it::Binomial) = binomial(it.n,it.k)
 
 subsets(xs,k) = Binomial(xs,length(xs),k)
 
-start(it::Binomial) = (collect(Int64, 1:it.k), false)
+start(it::Binomial) = (collect(Int64, 1:it.k), it.k>it.n ? true : false)
 
 function next(it::Binomial, state::Tuple{Array{Int64,1}, Bool})
     idx = state[1]
