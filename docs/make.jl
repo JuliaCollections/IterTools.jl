29155ac0a5eaff309ae3fe1c3bcc2a688f1eb025
Eric Davies <iamed2@gmail.com>
2018-06-25 11:47:07 -0500

    Docs run on 0.7 now

--- docs/make.jl
+++ docs/make.jl
@@ -12,7 +12,7 @@ makedocs(
 deploydocs(
     repo = "github.com/JuliaCollections/IterTools.jl.git",
     target = "build",
-    julia  = "0.6",
+    julia  = "0.7",
     osname = "linux",
     deps = nothing,
     make = nothing
