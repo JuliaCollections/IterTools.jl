2c39fbd524beaf213f0242f1051fce1ebbb5ade5
Szymon Bęczkowski <szymon.beczkowski@me.com>
2021-12-06 22:39:50 +0100

    bump Documenter

--- docs/make.jl
+++ docs/make.jl
@@ -2,18 +2,13 @@ using Documenter, IterTools
 
 makedocs(
     modules = [IterTools],
-    format = :html,
     sitename = "IterTools",
-    pages = Any[
+    pages = [
         "Introduction" => "index.md",
         "Function index" => "functionindex.md"
         ])
 
 deploydocs(
     repo = "github.com/JuliaCollections/IterTools.jl.git",
-    target = "build",
-    julia  = "1.0",
-    osname = "linux",
-    deps = nothing,
-    make = nothing
+    push_preview = true,
 )
